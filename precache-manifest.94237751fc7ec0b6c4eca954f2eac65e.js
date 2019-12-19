73ff45aff5ba2b73b836a972c6d6fd25003507a9
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-19 22:24:20 +0900

    Updates

---
+++ precache-manifest.94237751fc7ec0b6c4eca954f2eac65e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cea156c43b96b01a19387c92a4b08f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a17ad1e24a2182734af",
+    "url": "/static/css/main.d772c9c1.chunk.css"
+  },
+  {
+    "revision": "469be083fa460bfa9432",
+    "url": "/static/js/2.809e7291.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.809e7291.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a17ad1e24a2182734af",
+    "url": "/static/js/main.e264fa7b.chunk.js"
+  },
+  {
+    "revision": "20fa1333f19546fbb51c",
+    "url": "/static/js/runtime-main.2614eaf0.js"
+  }
+]);
