4b00f160433a24e6b479f8822de5e4cee87fc7f7
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-20 13:37:21 +0900

    update table

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
+// import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+// import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+// import Paper from '@material-ui/core/Paper';
 
-import Todos from './components/Todos'
 
 
 const useStyles = makeStyles({
@@ -23,65 +22,27 @@ const useStyles = makeStyles({
     minWidth: 650,
   },
 });
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+
+function createData(id, title, code, date, price, share, rate, share_after_tax) {
+  return { id, title, code, date, price, share, rate, share_after_tax };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(1, '롯데리츠', '033590','2019-12', 6230, 84, 1.35, 71),
+  createData(2, '롯데리츠', '033590', '2020.06', 6230, 158, 2.545, 134),
+  createData(3, '롯데리츠', '033590', '2020.12.', 6230, 158, 2.54, 134),
+  createData(4, 'NH프라임리츠', '388100', '2020.05.', 6030, 125, 2.07, 106),
+  createData(5, 'NH프라임리츠', '388100', '2020.09.', 6030, 125, 2.07, 106),
+  createData(6, '신한알파리츠', '293940', '2020.03', 7570, 140, 1.85, 118),
+  createData(7, '신한알파리츠', '293940', '2020.09', 7570, 140, 1.85, 118),
+  createData(8, '이리츠코크렙', '088260', '2019.12', 6820, 169, 2.48, 143),
+  createData(9, '이리츠코크렙', '088260', '2020.06', 6820, 170, 2.49, 144),
+  createData(10, '이리츠코크렙', '088260', '2020.12', 6820, 170, 2.49, 144),
 ];
-
 
 export class App extends Component {
 
-  state = {
-    todos:[
-      {
-        id:1, title:'롯데리츠', code:'033590', date:'2019-12', price:6230,
-        share:84, rate:1.35, share_after_tax:71, completed:false
-      },
-      {
-        id:2, title:'롯데리츠', code:'033590', date:'2020.06',price:6230,
-        share:158, rate:2.545, share_after_tax:134, completed:false
-      },
-      {
-        id:3, title:'롯데리츠', code:'033590', date:'2020.12.', price:6230,
-        share:158, rate:2.54, share_after_tax:134, completed:false
-      },
-      {
-        id:4, title:'NH프라임리츠', code:'388100', date:'2020.05.', price:6030,
-        share:125, rate:2.07, share_after_tax:106, completed:false
-      },
-      {
-        id:5, title:'NH프라임리츠', code:'388100', date:'2020.09.', price:6030,
-        share:125, rate:2.07, share_after_tax:106, completed:false
-      },
-      {
-        id:6, title:'신한알파리츠', code:'293940', date:'2020.03', price:7570,
-        share:140, rate:1.85, share_after_tax:118, completed:false
-      },
-      {
-        id:7, title:'신한알파리츠', code:'293940', date:'2020.09', price:7570,
-        share:140, rate:1.85, share_after_tax:118, completed:false
-      },
-      {
-        id:8, title:'이리츠코크렙', code:'088260', date:'2019.12', price:6820,
-        share:169, rate:2.48, share_after_tax:143, completed:false
-      },
-      {
-        id:9, title:'이리츠코크렙', code:'088260', date:'2020.06', price:6820,
-        share:170, rate:2.49, share_after_tax:144, completed:false
-      },
-      {
-        id:10, title:'이리츠코크렙', code:'088260', date:'2020.12', price:6820,
-        share:170, rate:2.49, share_after_tax:144, completed:false
-      },
-    ]
-  }
+  
 
   
   render() {
@@ -93,9 +54,32 @@ export class App extends Component {
             <Header/>
             <Route exact path="/" render={props => (
               <React.Fragment>
-                
-                {'리츠명'} {'배당시기'} {'현재주가'} {'배당금'} {'배당률'} {'배당금(세후)'}
-                <Todos todos={this.state.todos}></Todos>
+
+                  <TableHead>
+                    <TableRow rowSpan="2">
+                      <TableCell align="center">리츠명</TableCell>
+                      <TableCell align="right">배당시기</TableCell>
+                      <TableCell align="right">
+                        현재주가(원)
+                        </TableCell>
+                      <TableCell align="right">배당금(원)</TableCell>
+                      <TableCell align="right">배당률</TableCell>
+                      <TableCell align="right">배당금(원,세후)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map(row => (
+                      <TableRow key={row.id}>
+                        <TableCell align="left">{row.title}\n{'\n'}
+                        {row.code}</TableCell>
+                        <TableCell align="right">{row.date}</TableCell>
+                        <TableCell align="right">{row.price}</TableCell>
+                        <TableCell align="right">{row.share}</TableCell>
+                        <TableCell align="right">{row.rate}</TableCell>
+                        <TableCell align="right">{row.share_after_tax}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
 
               </React.Fragment>
             )}></Route>
